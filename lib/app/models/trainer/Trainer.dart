a3af6424119e546e97df62372da46ce5eb473784
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-01-29 15:33:50 +1000

    add factory constructor fromSheet

--- lib/app/models/trainer/Trainer.dart
+++ lib/app/models/trainer/Trainer.dart
@@ -6,4 +6,28 @@ class Trainer {
   String middlename;
 
   Trainer(this.firstname, this.lastname, this.middlename);
+
+  // фабричный конструктор для создания тренера из таблицы (Excel-файла)
+  // пример ФИО тренера: Пупкин В.А.
+  factory Trainer.fromSheet(String rawFullname) {
+    final parts = rawFullname.trim().split(" ");
+
+    if (parts.length != 2) {
+      throw FormatException("Неверный формат ФИО: '$rawFullname'. Ожидается 'Фамилия И.О.'");
+    }
+
+    final initialsPart = parts[1];
+
+    final initials = initialsPart.split(".").where((s) => s.isNotEmpty).toList();
+
+    if (initials.length != 2) {
+      throw FormatException("Не удалось разобрать инициалы в: '$initialsPart'. Ожидается формат 'И.О.'");
+    }
+
+    final lastname = parts[0];
+    final firstname = initials[0];
+    final middlename = initials[1];
+
+    return Trainer(lastname, firstname, middlename);
+  }
 }
