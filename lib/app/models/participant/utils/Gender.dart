bb9ef77f1dd3779a34436b0c73d995baadac7372
Kenedy228 <zhuravleffdanilka2004@mail.ru>
2026-01-29 16:25:17 +1000

    add tests for gender

--- lib/app/models/participant/utils/Gender.dart
+++ lib/app/models/participant/utils/Gender.dart
@@ -1,8 +1,23 @@
-
 // пол участника
 enum Gender {
   male,
-  female
+  female;
+
+  // фабричный метод для конвертации строкового значения (из Excel) в перечисление
+  factory Gender.fromSheet(String rawGender) {
+    switch (rawGender) {
+      case "м":
+      case "М":
+        return Gender.male;
+      case "ж":
+      case "Ж":
+        return Gender.female;
+      default:
+        throw FormatException(
+          "неудалось получить пол участника в: '$rawGender'. возможные значения: 'м, М, ж, Ж'",
+        );
+    }
+  }
 }
 
 // расширение для получения строкового описания значения Gender
---
+++ test/app/models/participant/utils/gender_test.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tournament_app/app/models/participant/utils/Gender.dart';
+
+void main() {
+  group("Gender.resolveGenderFromString", () {
+    test("resolving female gender from correct string throws no errors", () {
+      final inputs = ["ж", "Ж"];
+
+      for (String input in inputs) {
+        final got = Gender.fromSheet(input);
+        expect(got, equals(Gender.female));
+      }
+    });
+
+    test("resolving male gender from correct string throws no errors", () {
+      final inputs = ["м", "М"];
+
+      for (String input in inputs) {
+        final got = Gender.fromSheet(input);
+        expect(got, equals(Gender.male));
+      }
+    });
+
+    test("resolving gender from incorrect string throws FormatException", () {
+      final input = "Я";
+
+      expect(
+        () => Gender.fromSheet(input),
+        throwsA(
+          isA<FormatException>().having(
+            (e) => e.message,
+            'message',
+            "неудалось получить пол участника в: '$input'. возможные значения: 'м, М, ж, Ж'",
+          ),
+        ),
+      );
+    });
+  });
+}
