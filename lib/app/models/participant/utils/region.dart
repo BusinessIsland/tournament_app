2f1d9a340dfd8ec3bae4a251a951dcf73c0cd331
Данил Журавлев <zhuravleffdanilka2004@mail.ru>
2026-02-04 23:42:49 +1000

    change region.dart

--- lib/app/models/participant/utils/region.dart
+++ lib/app/models/participant/utils/region.dart
@@ -1,20 +1,19 @@
 class Region {
+  static const String _defaultValue = "не указано";
   final String value;
 
   Region(this.value);
 
   factory Region.withValidation(String? raw) {
     if (raw == null) {
-      return Region("");
+      return Region(_defaultValue);
     }
 
     return Region(raw.trim());
   }
 
   @override
-  String toString() {
-    return 'Region{value: $value}';
-  }
+  String toString() => value;
 
   @override
   bool operator ==(Object other) =>
